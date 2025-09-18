3e3506e20cdaaa50be4a4c7536542a9cedb8167d
Hadi Sheayto <hadi.sheayto@bmb.com.lb>
2025-09-18 13:40:00 +0300

    Revert "edit file4.py and file5.py"
    This reverts commit 1903e85f583985061b20f6532d64780fb16b6aa4.

--- ex3/file4.py
+++ ex3/file4.py
@@ -1,2 +1,1 @@
 print("file4 is back")
-print("i am editing this file to use it in the stetch task")
--- ex3/file5.py
+++ ex3/file5.py
@@ -1,2 +1,1 @@
 print("file5.py is created")
-print("i am editing this file to use it in the stetch task")
